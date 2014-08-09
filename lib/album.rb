5c8dce4fad2351d91eb0f33273358439109d4393
Dustin Lewis <d.lew23@gmail.com>
2014-08-09 16:38:38 -0700

    add save method for albums.

--- lib/album.rb
+++ lib/album.rb
@@ -1,9 +1,18 @@
 class Album
 
-attr_reader :name
+  attr_reader :name
+  @@albums = []
 
-def initialize(title)
-  @name = title
-end
+  def initialize(title)
+    @name = title
+  end
+
+  def self.all
+    @@albums
+  end
+
+  def save
+    @@albums << self
+  end
 
 end
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -7,4 +7,10 @@ describe Album do
     test_album.should be_an_instance_of Album
   end
 
+  it 'saves the album' do
+    test_album = Album.new("Is This It")
+    test_album.save
+    expect(Album.all).to eq [test_album]
+  end
+
 end
